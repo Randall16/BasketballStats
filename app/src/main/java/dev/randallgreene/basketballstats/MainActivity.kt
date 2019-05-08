eb0fbac5579614c086025ad11d662963fe37540d
Randall16 <randallgr5@outlook.com>
2019-05-08 08:04:10 -0400

    Initial commit

---
+++ app/src/main/java/dev/randallgreene/basketballstats/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.randallgreene.basketballstats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
