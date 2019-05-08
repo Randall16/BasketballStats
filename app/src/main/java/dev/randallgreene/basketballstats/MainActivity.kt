d2cdfd662a38f6582aee1b0be90ecfc14a13f39b
Randall16 <randallgr5@outlook.com>
2019-05-08 09:23:15 -0400

    added Jsoup library and sample html file to parse

--- app/src/main/java/dev/randallgreene/basketballstats/MainActivity.kt
+++ app/src/main/java/dev/randallgreene/basketballstats/MainActivity.kt
@@ -2,13 +2,31 @@ package dev.randallgreene.basketballstats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import org.jsoup.Jsoup
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val file = "steveNash.html"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val f = baseContext!!.assets.open(file)
+        val doc = Jsoup.parse(f, null, f.toString())
+
+        val table = doc.select("table[id=per_game]")
+
+
+        for(row in table.select("tr")) {
+            val tds = row.select("td")
+            if (tds.size > 6) {
+                Log.d("output", tds[0].text() + ":" + tds[1].text() + " 3% = " + tds[12].text())
+            }
+        }
 
     }
 }
